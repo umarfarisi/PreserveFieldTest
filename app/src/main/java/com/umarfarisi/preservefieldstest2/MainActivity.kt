bb499aabef6fe0bc2e44b3a259b37a096079ddbb
Muhammad Umar Farisi <pharese@gmail.com>
2019-05-26 14:39:32 +0700

    implement PreserveFields in MainActivity

--- app/src/main/java/com/umarfarisi/preservefieldstest2/MainActivity.kt
+++ app/src/main/java/com/umarfarisi/preservefieldstest2/MainActivity.kt
@@ -3,13 +3,23 @@ package com.umarfarisi.preservefieldstest2
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.umarfarisi.preservefield.PreserveField
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val fullClassPath = this::class.java.name
+    private val preserveField: PreserveField = PreserveField()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val getter: PreserveField.Getter = preserveField.clearDataAndGetFields(savedInstanceState != null)
+        val text: String? = getter.get(fullClassPath + "text", null)
+
+        text?.let(tvText::setText)
 
         title = "Main Activity"
 
@@ -22,4 +32,11 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        preserveField.putField(fullClassPath + "text", tvText.text.toString())
+        preserveField.save()
+    }
+
 }
